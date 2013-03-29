36176cb227913b826826fe7500054d1cfa85c28a
lucio cossio <lucio.cossio@gmail.com>
2013-03-29 19:04:13 -0300

    A test that don't close window.

--- ClassClientTest/src/com/luciocossio/classserviceclient/test/integration/PresentationTest.java
+++ ClassClientTest/src/com/luciocossio/classserviceclient/test/integration/PresentationTest.java
@@ -20,6 +20,21 @@ public class PresentationTest extends TestCase {
 	
 	public final String serverUrl = "http://10.1.1.4:8880/";
 	public final String fileName = "C:/Users/lucioc/Dropbox/Public/Mestrado/Dissertacao/PEP/PEP_posM.pptx";
+		
+	public void testUploadStartPresentation()
+	{
+		RESTJsonClient jsonClient = new RESTApacheClient();
+		PresentationClient client = new PresentationClient(jsonClient, serverUrl);
+		
+		File file = new File(fileName);
+		ResultMessage actual = client.uploadFile(file, "presentation.pptx");
+		ResultMessage expected = new ResultMessage("File was uploaded to server", true);
+		Assert.assertEquals(expected.getMessage(), actual.getMessage());
+		
+		actual = client.startPresentation("presentation.pptx");
+		expected = new ResultMessage("Presentation has been started", true);
+		Assert.assertEquals(expected, actual);
+	}
 	
 	public void testUploadStartNextPreviousClosePresentation()
 	{
